4aeb26769cdf7a59e0b5b62a90c990df73f7011f
Andrey <andrey0800770@gmail.com>
2025-06-24 17:41:02 -0300

    initial commit: import android_device_samsung_star2qlte

---
+++ BoardConfig.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include the common OEM chipset BoardConfig.
+include device/samsung/sdm845-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/star2qlte
+
+# Display
+TARGET_SCREEN_DENSITY := 480
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineage_star2qlte_defconfig
